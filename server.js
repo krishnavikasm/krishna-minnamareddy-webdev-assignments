1db9cd18bd1d1df3a0fb299c54fabc5d02fde826
krishna Minnamareddy <krishnavikas@ccs.neu.edu>
2017-07-09 03:49:23 -0400

    readme file, change the port to listen to http conns
    WIP
    
    wip
    
    wip
    
    WIP
    
    WIP

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var app = require('./express');
 var express = app.express;
 var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -8,4 +11,4 @@ app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
 
-app.listen(3000);
+app.listen(app.get('port'));
