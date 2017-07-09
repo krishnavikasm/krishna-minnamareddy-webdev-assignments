bc3e76800dff508c36dccbf5b730c41565660a33
krishna Minnamareddy <krishnavikas@ccs.neu.edu>
2017-07-09 04:06:22 -0400

    wip

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-var app = require('./express');
-var express = app.express;
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+var express = require('express')
+var app = express()
 
-require("./test/app");
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-app.listen(3000);
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
