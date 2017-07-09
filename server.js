9c31af71c1a8624c64a7814c0a9880cd9f56028b
krishna Minnamareddy <krishnavikas@ccs.neu.edu>
2017-07-09 04:21:06 -0400

    wip

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-
 var express = require('express')
 var app = express()
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
+require("./test/app");
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
